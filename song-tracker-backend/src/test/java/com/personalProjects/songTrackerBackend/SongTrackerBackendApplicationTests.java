7c9511de1c7aa864ea554dbd2e1e31c17e2c4ab4
Dylan Winters <163654886+DylanJPW@users.noreply.github.com>
2026-06-19 16:37:26 +0100

    Initial Setup

---
+++ song-tracker-backend/src/test/java/com/personalProjects/songTrackerBackend/SongTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProjects.songTrackerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
