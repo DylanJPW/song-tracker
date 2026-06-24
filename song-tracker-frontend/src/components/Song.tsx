766c9a55e9914c169254662be0cb3c438ab31e02
Dylan Winters <djpwinters2002@gmail.com>
2026-06-24 21:20:03 +0100

    Display links as clickable urls

--- song-tracker-frontend/src/components/Song.tsx
+++ song-tracker-frontend/src/components/Song.tsx
@@ -14,7 +14,19 @@ export function Song({ name, singer, dateAdded, links, notes }: SongProps) {
       <td className="border border-gray-300 px-4 py-2">
         {dateAdded.toDateString()}
       </td>
-      <td className="border border-gray-300 px-4 py-2">{links}</td>
+      <td className="border border-gray-300 px-4 py-2">
+        {links.map((link) => (
+          <a
+            className="block"
+            href={link}
+            key={link}
+            rel="noreferrer"
+            target="_blank"
+          >
+            {link}
+          </a>
+        ))}
+      </td>
       <td className="border border-gray-300 px-4 py-2">{notes}</td>
     </tr>
   );
