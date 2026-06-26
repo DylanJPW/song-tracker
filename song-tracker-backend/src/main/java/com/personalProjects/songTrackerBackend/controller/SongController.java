8dacd578488ac2dc5247791585d6fe2317ea816e
Dylan Winters <djpwinters2002@gmail.com>
2026-06-26 13:28:45 +0100

    Adds SongDTO and CRUD endpoints

--- song-tracker-backend/src/main/java/com/personalProjects/songTrackerBackend/controller/SongController.java
+++ song-tracker-backend/src/main/java/com/personalProjects/songTrackerBackend/controller/SongController.java
@@ -1,14 +1,15 @@
 package com.personalProjects.songTrackerBackend.controller;
 
 import com.personalProjects.songTrackerBackend.model.Song;
+import com.personalProjects.songTrackerBackend.model.SongDTO;
 import com.personalProjects.songTrackerBackend.service.SongService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,9 +19,48 @@ public class SongController {
     @Autowired
     private SongService songService;
 
-    @GetMapping()
-    public ResponseEntity<List<Song>> getAllSongs() {
-        List<Song> songs = songService.getAllSongs();
-        return new ResponseEntity<>(songs, HttpStatus.OK);
+    @GetMapping
+    public List<Song> getAllSongs() {
+        return songService.getAllSongs();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Song> getSong(@PathVariable int id) {
+        return ResponseEntity.of(songService.getSong(id));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteSong(@PathVariable int id) {
+        if (songService.deleteSong(id)) {
+            return ResponseEntity.status(HttpStatus.OK).build();
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Song> createSong(@RequestBody SongDTO songDTO) {
+        Song newSong = songService.createSong(
+                new Song(
+                        songDTO.getName(),
+                        songDTO.getArtist(),
+                        songDTO.getLinks(),
+                        songDTO.getNotes()
+                )
+        );
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newSong.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(newSong);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Song> updateSong(@PathVariable int id, @RequestBody SongDTO newSong) {
+        return songService.updateSong(id, newSong)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
