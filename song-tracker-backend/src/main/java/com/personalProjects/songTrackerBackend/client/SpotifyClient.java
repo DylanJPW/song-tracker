42d4f41372600f069a158707101b6342802f5b3c
Dylan Winters <djpwinters2002@gmail.com>
2026-07-08 14:55:33 +0100

    Adds test spotify secrets and moves SpotifyApi auth to client

--- song-tracker-backend/src/main/java/com/personalProjects/songTrackerBackend/client/SpotifyClient.java
+++ song-tracker-backend/src/main/java/com/personalProjects/songTrackerBackend/client/SpotifyClient.java
@@ -1,24 +1,41 @@
 package com.personalProjects.songTrackerBackend.client;
 
-import com.personalProjects.songTrackerBackend.service.SpotifyTokenService;
 import org.springframework.stereotype.Component;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
 import se.michaelthelin.spotify.model_objects.specification.Paging;
 import se.michaelthelin.spotify.model_objects.specification.Track;
 
+import java.time.Instant;
+
 @Component
 public class SpotifyClient {
 
     private final SpotifyApi spotifyApi;
-    private final SpotifyTokenService tokenService;
 
-    public SpotifyClient(
-            SpotifyApi spotifyApi,
-            SpotifyTokenService tokenService) {
+    private Instant expiresAt;
+
+    public SpotifyClient(SpotifyApi spotifyApi) {
 
         this.spotifyApi = spotifyApi;
-        this.tokenService = tokenService;
+    }
+
+    public SpotifyApi getSpotifyApi() throws Exception {
+
+        if (expiresAt == null || Instant.now().isAfter(expiresAt)) {
+
+            ClientCredentials credentials =
+                    spotifyApi.clientCredentials()
+                            .build()
+                            .execute();
+
+            spotifyApi.setAccessToken(credentials.getAccessToken());
+
+            expiresAt = Instant.now()
+                    .plusSeconds(credentials.getExpiresIn() - 60);
+        }
+
+        return spotifyApi;
     }
 
     public ClientCredentials getClientCredentials() throws Exception {
@@ -29,16 +46,11 @@ public class SpotifyClient {
 
     public Paging<Track> searchTracks(String query) throws Exception {
 
-        SpotifyApi api = tokenService.getSpotifyApi();
+        SpotifyApi api = getSpotifyApi();
 
         return api.searchTracks(query)
                 .limit(10)
                 .build()
                 .execute();
-    }
-
-
-    public SpotifyApi getSpotifyApi() {
-        return spotifyApi;
     }
 }
