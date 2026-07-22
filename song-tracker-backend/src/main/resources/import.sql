903c91a54b7b54ad2a0dd6b4bb88ca44b2098daa
Dylan Winters <djpwinters2002@gmail.com>
2026-07-22 13:27:52 +0100

    Migrates from H2 to Supabase

--- song-tracker-backend/src/main/resources/import.sql
+++ song-tracker-backend/src/main/resources/import.sql
@@ -1,14 +1,16 @@
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Something in the Orange', 'American Heartbreak', 'Zach Bryan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Maine', 'Busyhead', 'Noah Kahan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Lonely Island', 'Lonely Island', 'Amble', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Stick Season', 'Stick Season', 'Noah Kahan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Northern Attitude', 'Stick Season', 'Noah Kahan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Dial Drunk', 'Stick Season', 'Noah Kahan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Heading South', 'Heading South', 'Zach Bryan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Oklahoma Smokeshow', 'American Heartbreak', 'Zach Bryan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Revival', 'American Heartbreak', 'Zach Bryan', '');
-INSERT INTO songs (title, album, artist, image_url) VALUES ('Burn, Burn, Burn', 'American Heartbreak', 'Zach Bryan', '');
+INSERT INTO songs (title, album, artist, image_url) VALUES
+    ('Something in the Orange', 'American Heartbreak', 'Zach Bryan', ''),
+    ('Maine', 'Busyhead', 'Noah Kahan', ''),
+    ('Lonely Island', 'Lonely Island', 'Amble', ''),
+    ('Stick Season', 'Stick Season', 'Noah Kahan', ''),
+    ('Northern Attitude', 'Stick Season', 'Noah Kahan', ''),
+    ('Dial Drunk', 'Stick Season', 'Noah Kahan', ''),
+    ('Heading South', 'Heading South', 'Zach Bryan', ''),
+    ('Oklahoma Smokeshow', 'American Heartbreak', 'Zach Bryan', ''),
+    ('Revival', 'American Heartbreak', 'Zach Bryan', ''),
+    ('Burn, Burn, Burn', 'American Heartbreak', 'Zach Bryan', '');
 
-INSERT INTO users (username, password) VALUES ('testuser', 'testpassword');
-INSERT INTO users (username, password) VALUES ('john_doe', 'password123');
-INSERT INTO users (username, password) VALUES ('jane_smith', 'password456');
+INSERT INTO users (username, password) VALUES
+   ('testuser', 'testpassword'),
+   ('john_doe', 'password123'),
+   ('jane_smith', 'password456');
