5e8f8ba7d7f624b8145bb86f61ea6cf979a3b97b
Dylan Winters <djpwinters2002@gmail.com>
2026-08-04 19:58:38 +0100

    Removes table layout from SongList and SongItem

--- song-tracker-frontend/src/components/SongItem.tsx
+++ song-tracker-frontend/src/components/SongItem.tsx
@@ -4,45 +4,44 @@ import type {Song} from '@/api/schemas/SongSchema'
 import {saveUserSong} from '@/api/userSongs'
 
 export function SongItem({title, artist, album, imageUrl, spotifyId}: Song) {
-	const saveSongMutation = useMutation({
-		mutationFn: saveUserSong,
-		onSuccess: data => {
-			toast(`Saved ${data.song.title}`, {
-				position: 'top-right',
-				autoClose: 5000,
-				hideProgressBar: true,
-				closeOnClick: false,
-				pauseOnHover: true,
-				draggable: true,
-				theme: 'light'
-			})
-		}
-	})
+  const saveSongMutation = useMutation({
+    mutationFn: saveUserSong,
+    onSuccess: data => {
+      toast(`Saved ${data.song.title}`, {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        theme: 'light'
+      })
+    }
+  })
 
-	function onClick() {
-		saveSongMutation.mutate(spotifyId as string)
-	}
+  function onClick() {
+    saveSongMutation.mutate(spotifyId as string)
+  }
 
-	return (
-		<tr className='font-medium'>
-			<td className='border border-gray-300 px-4 py-2'>
-				<img alt={title} height={100} src={imageUrl} width={100} />
-			</td>
-			<td className='border border-gray-300 px-4 py-2'>
-				<p>
-					{title} - {album}
-				</p>
-			</td>
-			<td className='border border-gray-300 px-4 py-2'>{artist}</td>
-			<td className='border border-gray-300 px-4 py-2'>
-				<button
-					className='cursor-pointer rounded-sm bg-blue-500 p-2 text-center hover:bg-blue-400'
-					onClick={onClick}
-					type='button'
-				>
-					Want to learn
-				</button>
-			</td>
-		</tr>
-	)
+  return (
+    <div className='flex grow flex-row gap-x-2 border-slate-400 border-b bg-slate-900 p-2'>
+      <img alt={title} height={100} src={imageUrl} width={100}/>
+      <div className='flex grow flex-col justify-between'>
+        <div>
+          <p>{title}</p>
+          <p>{album}</p>
+        </div>
+        <p className=''>{artist}</p>
+      </div>
+      {/*<div className='flex items-center'>*/}
+      {/*  <button*/}
+      {/*    className='cursor-pointer rounded-sm bg-blue-500 p-2 text-center hover:bg-blue-400'*/}
+      {/*    onClick={onClick}*/}
+      {/*    type='button'*/}
+      {/*  >*/}
+      {/*    Want to learn*/}
+      {/*  </button>*/}
+      {/*</div>*/}
+    </div>
+  )
 }
--- song-tracker-frontend/src/components/SongList.tsx
+++ song-tracker-frontend/src/components/SongList.tsx
@@ -7,29 +7,18 @@ interface SongListProps {
 
 export function SongList({songs}: SongListProps) {
   return (
-    <div className="flex place-content-center">
-      <table>
-        <thead>
-        <tr>
-          <th className="px-4 py-2">Album Cover</th>
-          <th className="px-4 py-2">Title</th>
-          <th className="px-4 py-2">Artist</th>
-        </tr>
-        </thead>
-        <tbody>
-        {songs.map(({id, title, artist, album, imageUrl, spotifyId}) => (
-          <SongItem
-            album={album}
-            artist={artist}
-            id={id}
-            imageUrl={imageUrl}
-            key={id}
-            spotifyId={spotifyId}
-            title={title}
-          />
-        ))}
-        </tbody>
-      </table>
+    <div className="flex flex-col place-content-center">
+      {songs.map(({id, title, artist, album, imageUrl, spotifyId}) => (
+        <SongItem
+          album={album}
+          artist={artist}
+          id={id}
+          imageUrl={imageUrl}
+          key={id}
+          spotifyId={spotifyId}
+          title={title}
+        />
+      ))}
     </div>
   );
 }
