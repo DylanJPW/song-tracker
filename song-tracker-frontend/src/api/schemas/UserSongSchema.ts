f3a48c8b9a354bdb5b8b2992d2d339560a17b2d2
Dylan Winters <djpwinters2002@gmail.com>
2026-08-19 17:09:24 +0100

    Adds dateAdded field to UserSong schema and adds UpdateUserSong interface

--- song-tracker-frontend/src/api/schemas/UserSongSchema.ts
+++ song-tracker-frontend/src/api/schemas/UserSongSchema.ts
@@ -1,5 +1,6 @@
 import * as v from 'valibot'
 import {Song} from '@/api/schemas/SongSchema'
+import {apiDate} from "@/api/schemas/common";
 
 export const songStatuses = ['WANT_TO_LEARN', 'LEARNING', 'LEARNED'] as const
 export type SongStatus = (typeof songStatuses)[number]
@@ -12,6 +13,7 @@ export const songStatusLabels: Record<SongStatus, string> = {
 
 export const UserSong = v.object({
   capo: v.union([v.number(), v.null()]),
+  dateAdded: apiDate,
   difficultyRating: v.union([v.number(), v.null()]),
   id: v.number(),
   song: Song,
@@ -20,3 +22,11 @@ export const UserSong = v.object({
 export type UserSong = v.InferOutput<typeof UserSong>
 
 export const UserSongs = v.array(UserSong)
+
+export interface UpdateUserSong {
+  capo?: number | null
+  difficultyRating?: number | null
+  id: number
+  notes?: string | null
+  status?: SongStatus
+}
