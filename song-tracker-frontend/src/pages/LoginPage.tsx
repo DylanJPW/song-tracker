43b9baed1f1fa12ecdc2fd743bce8b71e03c81c7
Dylan Winters <djpwinters2002@gmail.com>
2026-08-24 21:53:50 +0100

    Changes LoginPage background and adds Prettier

--- song-tracker-frontend/src/pages/LoginPage.tsx
+++ song-tracker-frontend/src/pages/LoginPage.tsx
@@ -2,8 +2,10 @@ import { AuthPanel } from "@/components/forms/AuthPanel";
 
 export function LoginPage() {
   return (
-    <div className="flex w-full grow justify-center self-center bg-slate-900 px-10 md:w-lg">
-      <AuthPanel />
+    <div className="flex grow justify-center">
+      <div className="self-center px-10 py-6 md:bg-slate-900 md:w-lg md:rounded-2xl">
+        <AuthPanel />
+      </div>
     </div>
   );
 }
